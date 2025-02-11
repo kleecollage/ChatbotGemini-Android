f300d4f6913201618d5c8e32e3611482ff3a5656
Klee Collage <klee.collage@gmail.com>
2025-02-11 15:16:02 -0600

    feat(components, viewModel): Implement bodyComponents and GeminiVM

--- app/src/main/java/com/kleecollage/geminichatbot/ui/theme/Color.kt
+++ app/src/main/java/com/kleecollage/geminichatbot/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val focusColor = Color(0xFF27252B)
+val unfocusedColor = Color(0xFF27252B)
+val backColor = Color(0XFF332F36)
--- app/src/main/java/com/kleecollage/geminichatbot/viewModel/GeminiViewModel.kt
+++ app/src/main/java/com/kleecollage/geminichatbot/viewModel/GeminiViewModel.kt
@@ -2,7 +2,11 @@ package com.kleecollage.geminichatbot.viewModel
 
 import androidx.lifecycle.ViewModel
 import com.google.ai.client.generativeai.GenerativeModel
+import com.kleecollage.geminichatbot.BuildConfig
 
 class GeminiViewModel: ViewModel() {
-    private val generativeModel = GenerativeModel 
+    private val generativeModel = GenerativeModel (
+        modelName = "gemini-1.5-flash",
+        apiKey = BuildConfig.apiKey
+    )
 }
