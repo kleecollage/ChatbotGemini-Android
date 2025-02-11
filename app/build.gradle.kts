8481520de0040d622e80ef0c9eae587e5ad1404e
Klee Collage <klee.collage@gmail.com>
2025-02-11 14:57:46 -0600

    Implement local properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -36,6 +37,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -57,5 +59,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
 }
---
+++ app/src/main/java/com/kleecollage/geminichatbot/viewModel/GeminiViewModel.kt
@@ -0,0 +1,8 @@
+package com.kleecollage.geminichatbot.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.google.ai.client.generativeai.GenerativeModel
+
+class GeminiViewModel: ViewModel() {
+    private val generativeModel = GenerativeModel 
+}
